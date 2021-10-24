db1d03d8b5f161f5f026734ba7a4c56ede507ae7
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2021-10-24 17:01:25 -0400

    This first commit is creating an expense component that taps into an array of expense objects to display in the browser.

---
+++ expense-app/src/App.js
@@ -0,0 +1,59 @@
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Cell Phone Bill",
+      amount: 126.0,
+      date: new Date(2021, 10, 10),
+    },
+    {
+      id: "e2",
+      title: "Con Ed Bill",
+      amount: 115.52,
+      date: new Date(2021, 10, 19),
+    },
+    {
+      id: "e3",
+      title: "Optimum Internet",
+      amount: 84.0,
+      date: new Date(2021, 10, 30),
+    },
+    {
+      id: "e4",
+      title: "National Grid Gas Bill",
+      amount: 19.26,
+      date: new Date(2021, 10, 25),
+    },
+  ];
+  return (
+    <div>
+      <h2>Expense App - Let's get it started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+      
+    </div>
+  );
+}
+
+export default App;
